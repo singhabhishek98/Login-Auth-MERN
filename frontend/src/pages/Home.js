cd976236327d760da9824152442438a996d02e58
Abhishek Singh <104780355+singhabhishek98@users.noreply.github.com>
2025-03-05 18:45:22 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "http://deploy-mern-app-1-api.vercel.app/products";
+            const url = 'https://login-auth-mern.vercel.app/products';
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
